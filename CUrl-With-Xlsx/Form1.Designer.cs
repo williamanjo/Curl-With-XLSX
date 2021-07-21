2b235f87c3e8b71f35bc98b531be789f016dda00
williamanjo <william.anjodaguarda20@gmail.com>
2021-07-21 09:01:43 -0300

    First Commit :rocket:

---
+++ CUrl-With-Xlsx/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿
+namespace CUrl_With_Xlsx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openExcel = new System.Windows.Forms.OpenFileDialog();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBoxExcelSheetNames = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.Name = "progressBar1";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Name = "label1";
+            // 
+            // openExcel
+            // 
+            resources.ApplyResources(this.openExcel, "openExcel");
+            this.openExcel.FilterIndex = 2;
+            // 
+            // progressBar2
+            // 
+            resources.ApplyResources(this.progressBar2, "progressBar2");
+            this.progressBar2.Name = "progressBar2";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // comboBoxExcelSheetNames
+            // 
+            this.comboBoxExcelSheetNames.FormattingEnabled = true;
+            resources.ApplyResources(this.comboBoxExcelSheetNames, "comboBoxExcelSheetNames");
+            this.comboBoxExcelSheetNames.Name = "comboBoxExcelSheetNames";
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            resources.ApplyResources(this.textBox4, "textBox4");
+            this.textBox4.Name = "textBox4";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // textBox5
+            // 
+            resources.ApplyResources(this.textBox5, "textBox5");
+            this.textBox5.Name = "textBox5";
+            // 
+            // textBox6
+            // 
+            resources.ApplyResources(this.textBox6, "textBox6");
+            this.textBox6.Name = "textBox6";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // checkBox2
+            // 
+            resources.ApplyResources(this.checkBox2, "checkBox2");
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // textBox7
+            // 
+            resources.ApplyResources(this.textBox7, "textBox7");
+            this.textBox7.Name = "textBox7";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBoxExcelSheetNames);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openExcel;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBoxExcelSheetNames;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
+
---
+++ CUrl-With-Xlsx/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Threading.Tasks;
+using OfficeOpenXml;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace CUrl_With_Xlsx
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openExcel.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                textBox1.Text = openExcel.FileName;
+                using (ExcelPackage xlPackage = new ExcelPackage(new FileInfo(@"" + textBox1.Text)))
+                {
+                    foreach (var worksheet in xlPackage.Workbook.Worksheets)
+                    {
+                        comboBoxExcelSheetNames.Items.Add(worksheet.Name);
+                    }
+                }
+                comboBoxExcelSheetNames.SelectedIndex = 0;
+                
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog
+            {
+                IsFolderPicker = true,
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            };
+            CommonFileDialogResult result = dialog.ShowDialog();
+            if (result.ToString() == "Ok")
+            {
+
+                textBox2.Text = dialog.FileName;
+            }
+            dialog = null;
+
+        }
+        public void WriteLog(string log,TextBox tb)
+        {
+                tb.AppendText(log + Environment.NewLine);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = String.Empty;
+            GetxmlsxAsync();
+
+        }
+
+        private async void GetxmlsxAsync() {
+            
+            using (ExcelPackage xlPackage = new ExcelPackage(new FileInfo(@"" + textBox1.Text)))
+            {
+
+                var myWorksheet = xlPackage.Workbook.Worksheets[comboBoxExcelSheetNames.SelectedIndex]; //select sheet here
+                var totalRows = myWorksheet.Dimension.End.Row;
+                var totalColumns = myWorksheet.Dimension.End.Column;
+
+                var ids = new List<string>();
+                var uris = new List<Uri>();
+                for (int rowNum = (checkBox1.Checked ? 2: 1); rowNum <= totalRows; rowNum++) //select starting row here
+                {
+                    if (Convert.ToString(myWorksheet.Cells[rowNum, Int32.Parse(textBox5.Text)].Value) != "-" && myWorksheet.Cells[rowNum, Int32.Parse(textBox5.Text)].Value != null && Convert.ToString(myWorksheet.Cells[rowNum, Int32.Parse(textBox5.Text)].Value) != "#N/D" && Uri.IsWellFormedUriString(Convert.ToString(myWorksheet.Cells[rowNum, Int32.Parse(textBox5.Text)].Value), UriKind.Absolute))
+                    {
+
+                        ids.Add(Convert.ToString(myWorksheet.Cells[rowNum, Int32.Parse(textBox4.Text)].Value));
+
+                        uris.Add(new Uri(Convert.ToString(myWorksheet.Cells[rowNum, Int32.Parse(textBox5.Text)].Value)));
+                    }
+                }
+                myWorksheet = null;
+                progressBar2.Maximum = ids.Count;
+                progressBar2.Value = 0;
+                xlPackage.Dispose();
+                label2.Text = "Arquivos: 0 de " + ids.Count();
+                foreach (var doc in ids.Zip(uris,Tuple.Create))
+                {
+                   await Download(doc);
+                    progressBar2.Value += 1;
+                    label2.Text = "Arquivos: "+ progressBar2.Value + " de " + ids.Count();
+                }
+                if (checkBox2.Checked)
+                {
+                    System.Diagnostics.Process.Start("explorer.exe", textBox2.Text);
+                }
+
+            }
+        }
+
+
+        public async Task Download(Tuple<String, Uri> doc)
+        {
+            // Create a new WebClient instance.
+
+            WebClient webClient = new WebClient
+            {
+                Proxy = null
+            };
+            progressBar1.Value = 0;
+            progressBar1.Maximum = 100;
+            webClient.DownloadProgressChanged += (s, e) =>
+            {
+                if (e.ProgressPercentage > 0 && label1.Text != "Completed:" + e.ProgressPercentage.ToString() + "%")
+                {
+                    progressBar1.Value = e.ProgressPercentage;
+                    label1.Text = "Completed:" + progressBar1.Value.ToString() + "%";
+                    textBox3.Text = textBox3.Text.Remove(textBox3.Text.LastIndexOf(Environment.NewLine));
+                    textBox3.AppendText(Environment.NewLine + "Completed:" + progressBar1.Value.ToString() + "% ");
+                }
+            };
+            webClient.DownloadFileCompleted += (s, e) =>
+            {
+                WriteLog(Environment.NewLine + "Successfully Downloaded File \"" + doc.Item1 + "\" from \"" + doc.Item2 + "\" " + Environment.NewLine, textBox3);
+                WriteLog("Downloaded file saved in the following file system folder:" + Environment.NewLine + textBox2.Text, textBox3);
+            };
+            // Concatenate the domain with the Web resource filename.
+            WriteLog(Environment.NewLine + "Downloading File \"" + doc.Item1 + "\" from \"" + doc.Item2 + "\" ......." + Environment.NewLine, textBox3);
+            // Download the Web resource and save it into the current filesystem folder.
+            await webClient.DownloadFileTaskAsync(doc.Item2, textBox2.Text +"\\"+ doc.Item1 + (textBox6.Text != "" ? "-"+ textBox6.Text : "" ) + textBox7.Text);
+            webClient.Dispose();
+            label1.Text = "";
+            
+        }
+
+    }
+}
